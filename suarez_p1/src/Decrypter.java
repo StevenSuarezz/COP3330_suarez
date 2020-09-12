eae7982c92d477614e77a4334d936e4b9220b5d8
Steven Suarez <suarezsteven30@gmail.com>
2020-09-12 16:45:38 -0400

    Finished a working decrypter class as per instructions

--- suarez_p1/src/Decrypter.java
+++ suarez_p1/src/Decrypter.java
@@ -1,6 +1,32 @@
 public class Decrypter {
     public String decrypt(String s){
+        int[] digits = convertToIntArray(s);
+        String decryptedValue = decryptionAlgorithm(digits);
+        return decryptedValue;
+    }
 
-        return "";
+    private int[] convertToIntArray(String s){
+        int[] digits = new int[s.length()];
+        for (int i = 0; i < digits.length; i++){
+            digits[i] = Integer.parseInt(s.substring(i, i+1));
+        }
+        return digits;
+    }
+
+    private String decryptionAlgorithm(int[] digits){
+        // Step 1: Swap the first digit with the third, and swap the second digit with the fourth
+        for (int i = 0; i < digits.length / 2; i++){
+            int temp = digits[i];
+            digits[i] = digits[i+2];
+            digits[i+2] = temp;
+        }
+
+        // Step 2: Replace each digit with the result of adding 7 to the digit and getting the remainder after dividing the new value by 10
+        for (int i = 0; i < digits.length; i++){
+            digits[i] = (digits[i] + 3) % 10;
+        }
+
+        // Step 3: Return the encrypted integer as a String
+        return "" + digits[0] + digits[1] + digits[2] + digits[3];
     }
 }

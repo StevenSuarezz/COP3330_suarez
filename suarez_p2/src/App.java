9ee4969949565505c29fb1459392d874bebfc2fd
Steven Suarez <suarezsteven30@gmail.com>
2020-09-28 12:56:15 -0400

    Finished App class methods

--- suarez_p2/src/App.java
+++ suarez_p2/src/App.java
@@ -1,25 +1,67 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class App {
 
+    private static final Scanner scan = new Scanner(System.in);
+
+    // Calls scan.nextLine(); to clear the input buffer
+    private static void clearInputBuffer(){
+        scan.nextLine();
+    }
+
+    // Asks the user if they have more information to input
     private static boolean moreInput(){
-        return false;
+        System.out.println("Do you wish to enter more data? Enter 'Y' or 'N'");
+
+        String ans = scan.nextLine();
+        return ans.toLowerCase().equals("y");
     }
 
+    // Asks the user to enter a height. Only accepts positive values
     private static double getUserHeight(){
-        return 0;
+        System.out.println("Please enter a valid height in inches: ");
+
+        double height = scan.nextDouble();
+        clearInputBuffer();
+
+        if(height <= 0){
+            return getUserHeight();
+        } else {
+            return height;
+        }
     }
 
+    // Asks the user to enter a weight. Only accepts positive values
     private static double getUserWeight(){
-        return 0;
+        System.out.println("Please enter a valid weight in pounds: ");
+
+        double weight = scan.nextDouble();
+        clearInputBuffer();
+
+        if(weight <= 0){
+            return getUserWeight();
+        } else {
+            return weight;
+        }
     }
 
+    // Displays the BMI score and category of an entry
     private static void displayBmiInfo(BodyMassIndex bmi){
-
+        System.out.println("Score: " + bmi.calculateBmiScore() + ", Category: " + bmi.calculateBmiCategory() + "\n");
     }
 
+    // Averages the BMI scores in the array list that is passed in
     private static void displayBmiStatistics(ArrayList<BodyMassIndex> bmiData){
+        double scoreSum = 0.0;
 
+        for (BodyMassIndex bmi : bmiData){
+            scoreSum += bmi.calculateBmiScore();
+        }
+
+        double average = scoreSum / bmiData.size();
+
+        System.out.println("The average BMI score is: " + average);
     }
 
     public static void main(String[] args) {
